fe13cae2f83495197e5c4c05dff620ac9f053861
waheed khan <wahid---@gmail.com>
2021-04-27 18:32:28 +0530

    add lst collections

---
+++ src/com/simplilearn/collections/list/EmployeeList.java
@@ -0,0 +1,28 @@
+package com.simplilearn.collections.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeList {
+
+	public static void main(String[] args) {
+		// create multiple employee objects
+		Employee employee1 = new Employee(1001, "John Smith", 238237.342);
+		Employee employee2 = new Employee(1002, "Marry Smith", 138237.342);
+		Employee employee3 = new Employee(1003, "Mike Smith", 9878237.342);
+		Employee employee4 = new Employee(1004, "David Smith", 998237.342);
+		
+		List<Employee> listOfEmployee = new ArrayList<Employee>();
+		
+		listOfEmployee.add(employee1);
+		listOfEmployee.add(employee2);
+		listOfEmployee.add(employee3);
+		
+		// insert at index
+		listOfEmployee.add(2,employee4);
+		
+		System.out.println(listOfEmployee);
+		System.out.println(listOfEmployee.get(2));
+	}
+	
+}
---
+++ src/com/simplilearn/collections/list/ListDemo.java
@@ -0,0 +1,63 @@
+package com.simplilearn.collections.list;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Vector;
+
+public class ListDemo {
+
+	public static void main(String[] args) {
+
+		// List -> Order collection of data elements which allows duplicates elements.
+		// List (interface) -> implementation classes -> ArrayList, LinkedList , Vector , Stack
+		
+		//syntax:
+		List<String> list1 = new ArrayList<String>();  // not thread safe -> ordered data -> read operation
+		List<String> list2 = new LinkedList<String>();  // not thread safe -> ordered data -> update(insert/delete) operation
+		List<String> list3 = new Vector<String>();   // thread safe -> similar to arraylist.
+		List<String> list4 = new Stack<String>();    // old collection api // legacy collections
+		
+		// array list
+		List<String> listOfCompanies = new ArrayList<String>();
+		listOfCompanies.add("DELL");
+		listOfCompanies.add("MCIT");
+		listOfCompanies.add("SIMPLILEARN");
+		listOfCompanies.add("WIPRO");
+		listOfCompanies.add("SIMPLILEARN");
+		// insert 
+		listOfCompanies.add(3,"INFOSYS");
+		System.out.println(listOfCompanies);
+		System.out.println(listOfCompanies.get(1));
+		
+		System.out.println("-----------------");
+		List<String> listOfCities = new LinkedList<String>();
+		listOfCities.add("MUMBAI");
+		listOfCities.add("NEWYORK");
+		listOfCities.add("PUNE");
+		listOfCities.add("NEWYORK");
+		listOfCities.add("HYDERABAD");
+		
+		System.out.println(listOfCities);
+		System.out.println(listOfCities.get(1));
+		
+		
+		System.out.println("-----------------");
+		List<String> listOfFruits = new Vector<String>();
+		listOfFruits.add("APPLE");
+		listOfFruits.add("ORANGE");
+		listOfFruits.add("KIWI");
+		listOfFruits.add("ORANGE");
+		listOfFruits.add("APPLE");
+		
+		System.out.println(listOfFruits);
+		System.out.println(listOfFruits.get(1));
+		
+		
+		
+		
+		
+	}
+
+}
